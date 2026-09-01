d4bc47adab9ccb09ed926b78476350efa2ad55d4
reb-mikealson <pratharajgupta@gmail.com>
2026-09-01 18:51:36 +0530

    data cleaning in progress

--- ml/dataClean.py
+++ ml/dataClean.py
@@ -2,3 +2,4 @@ import pandas as pd
 df = pd.read_csv("data/PhiUSIIL_Phishing_URL_Dataset.csv")
 id_cols = ["FILENAME", "URL", "Domain", "Title"]
 df_model = df.drop(columns=id_cols)
+print("Shape after removing duplicates:", df.shape)
