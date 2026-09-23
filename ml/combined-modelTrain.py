11cbd6b8eb2e5be4f474462bf3ba83be609887dd
reb-mikealson <pratharajgupta@gmail.com>
2026-09-23 14:17:50 +0530

    combined training

---
+++ ml/combined-modelTrain.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
